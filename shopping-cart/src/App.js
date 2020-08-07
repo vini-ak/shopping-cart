c80a1dff7d636c07ccfd9953217181f7e63740d0
Vinícius Vieira <vinicius.vdes@gmail.com>
2020-08-07 16:23:42 -0300

    Terminando a navbar

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
+import Home from './components/Home';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <nav>
-          <h2>Shopping Cart</h2>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Cart</a></li>
-          </ul>
-        </nav>
-      </header>
+      <Home />
     </div>
   );
 }
---
+++ shopping-cart/src/components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Home() {
+    return (
+            <header>
+                <div className="overlay"></div>
+                <nav>
+                <h2>Shopping Cart</h2>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#" className="basket-sharp">
+                    <ion-icon name="basket-sharp"></ion-icon>
+                    Cart
+                    </a></li>
+                </ul>
+                </nav>
+            </header>
+    );
+}
+
+export default Home;
