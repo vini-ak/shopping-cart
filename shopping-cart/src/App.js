40e8e65d66c90f01626cf530ada4a5fc3425fc0e
Vinícius <vinicius.vdes@gmail.com>
2020-08-07 07:39:29 -0300

    Colocando a navbar.

---
+++ shopping-cart/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <nav>
+          <h2>Shopping Cart</h2>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Cart</a></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
